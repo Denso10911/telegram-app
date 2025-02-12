2a5254c3c5839a997c82c31951a4b54531d27d2e
denso10911 <denso10911@gmail.com>
2025-02-12 11:55:41 +0200

    Remove server-side locale handling from layout
    Removed the `getLocale` function and the `lang` attribute in the HTML tag, simplifying the layout component. This shifts the responsibility for locale handling away from the server to focus on client-side logic only.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,5 @@
 "use client"
 import {PropsWithChildren, useEffect} from 'react';
-import type { Metadata } from 'next';
-import { getLocale } from 'next-intl/server';
 
 import { Root } from '@/components/Root/Root';
 import { I18nProvider } from '@/core/i18n/provider';
@@ -19,8 +17,6 @@ declare const Telegram: any;
 
 
 export default async function RootLayout({ children }: PropsWithChildren) {
-  const locale = await getLocale();
-
 
   useEffect(() => {
     // Переконаємося, що код виконується тільки на клієнті
@@ -46,7 +42,7 @@ export default async function RootLayout({ children }: PropsWithChildren) {
   }, []);
 
   return (
-    <html lang={locale}>
+    <html>
     <body>
     <I18nProvider>
       <Root>
